583710506687731a3c3ad53b0081b0641ed4bffd
Rahul Chintala <116490429+ChintalaR@users.noreply.github.com>
2022-12-23 20:35:51 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,15 @@
+function contact(){
+    document.getElementById('contact').style.cssText = 'display:block';
+    document.getElementById('social').style.cssText = 'display:none';
+    document.getElementById('coding').style.cssText = 'display:none';
+}
+function social(){
+    document.getElementById('contact').style.cssText = 'display:none';
+    document.getElementById('social').style.cssText = 'display:block';
+    document.getElementById('coding').style.cssText = 'display:none';
+}
+function coding(){
+    document.getElementById('contact').style.cssText = 'display:none';
+    document.getElementById('social').style.cssText = 'display:none';
+    document.getElementById('coding').style.cssText = 'display:block';
+}
